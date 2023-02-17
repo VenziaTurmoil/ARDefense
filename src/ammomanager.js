2a7ecde00bf8ef33f4fad68b5853151b0ed6192c
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-02-17 10:30:40 +0100

    ammo done

---
+++ src/ammomanager.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three';
+import { Euler, Vector3 } from 'three';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+export class AmmoManager {
+    constructor() {
+
+        this.scene = undefined
+        // ---- Ammo List ----
+        this.ammoArray = new Array();
+        // ---- Temporary variables ----
+        this.newAmmoMeshToCreate = undefined;
+
+        this.future_type = 'dart';
+
+        function loadData(that) {
+            new GLTFLoader()
+                .setPath('assets/models/')
+                .load('dartOnly__1_.glb', dartReader(that));
+        }
+
+
+        function dartReader(that) {
+            return (gltf) => {
+                that.newAmmoMeshToCreate = null;
+                that.newAmmoMeshToCreate = gltf.scene;
+            }
+        }
+
+        loadData(this);
+
+    }
+
+    setScene(scene) {
+        this.scene = scene;
+    }
+
+    addAmmo(camera, elapsedTime) {
+        // AMMO MESH
+        let ammo_mesh = this.newAmmoMeshToCreate.clone();
+
+        ammo_mesh.position.add(
+            new Vector3(camera.position.x, camera.position.y, camera.position.z))
+
+        let target = new THREE.Vector3(0, 0, - 1);
+        target.applyQuaternion(camera.quaternion);
+
+        ammo_mesh.lookAt(target)
+        this.scene.add(ammo_mesh);
+
+        if (this.future_type == 'dart') {
+            var newAmmo = new Dart(elapsedTime, target);
+            newAmmo.mesh = ammo_mesh;
+            this.ammoArray.push(newAmmo);
+        }
+    }
+
+    deleteAmmos(ammodelete_array) {
+        for (var i = 0; i < ammodelete_array.length; i++) {
+            const index = this.ammoArray.indexOf(ammodelete_array[i]);
+            if (index > -1) {
+                this.ammoArray.splice(index, 1);
+            }
+            this.scene.remove(ammodelete_array[i].mesh);
+        }
+    }
+
+    updateAmmosPositions(elapsed, delta) {
+        const ammodelete_array = new Array();
+        for (let i = 0; i < this.ammoArray.length; i++) {
+            if (this.ammoArray[i].birth + this.ammoArray[i].uptime < elapsed) {
+                ammodelete_array.push(this.ammoArray[i])
+            }
+            this.ammoArray[i].update_position(delta)
+        }
+        this.deleteAmmos(ammodelete_array, this.scene)
+    }
+}
+
+class Ammo {
+    constructor(elapsedTime, target) {
+        this.mesh = undefined;
+        this.uptime = undefined;
+        this.birth = elapsedTime;
+        this.speed = undefined;
+        this.target = target;
+    }
+
+    update_position(delta) {
+        let v = this.target.clone();
+        v.normalize().multiplyScalar(this.speed * delta);
+        this.mesh.position.add(v);
+    }
+
+}
+
+class Dart extends Ammo {
+    constructor(elapsedTime, target) {
+        super(elapsedTime, target);
+        this.uptime = 1000;
+        this.speed = 0.5;
+    }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,17 +2,18 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { ARButton } from 'three/addons/webxr/ARButton.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { AmmoManager } from './ammomanager';
 
 let container;
-let camera, scene, renderer;
+let camera, scene, renderer, clock;
 let controller1, controller2;
+
+let ammoMngr = new AmmoManager();
 
 let raycaster;
 
 const intersected = [];
 const tempMatrix = new THREE.Matrix4();
-
-let group;
 
 init();
 animate();
@@ -23,6 +24,7 @@ function init() {
   document.body.appendChild(container);
 
   scene = new THREE.Scene();
+  clock = new THREE.Clock();
 
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
   camera.position.set(0, 0, 3);
@@ -36,61 +38,6 @@ function init() {
   const light = new THREE.DirectionalLight(0xffffff);
   light.position.set(0, 6, 0);
   scene.add(light);
-
-  group = new THREE.Group();
-  scene.add(group);
-
-  const geometries = [
-    new THREE.BoxGeometry(0.2, 0.2, 0.2),
-    new THREE.ConeGeometry(0.2, 0.2, 64),
-    new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
-    new THREE.IcosahedronGeometry(0.2, 8),
-    new THREE.TorusGeometry(0.2, 0.04, 64, 32)
-  ];
-
-  for (let i = 0; i < 50; i++) {
-
-    const geometry = geometries[Math.floor(Math.random() * geometries.length)];
-    const material = new THREE.MeshStandardMaterial({
-      color: Math.random() * 0xffffff,
-      roughness: 0.7,
-      metalness: 0.0
-    });
-
-    const object = new THREE.Mesh(geometry, material);
-
-    object.position.x = Math.random() * 4 - 2;
-    object.position.y = Math.random() * 4 - 2;
-    object.position.z = Math.random() * 4 - 2;
-
-    object.rotation.x = Math.random() * 2 * Math.PI;
-    object.rotation.y = Math.random() * 2 * Math.PI;
-    object.rotation.z = Math.random() * 2 * Math.PI;
-
-    object.scale.setScalar(Math.random() + 0.5);
-
-    group.add(object);
-
-  }
-
-  let dart_mesh = null;
-
-  function loadData() {
-    console.log('0');
-    new GLTFLoader()
-      .setPath('assets/models/')
-      .load('dartmonkey__2_.glb', dartMonkeyReader);
-  }
-
-
-  function dartMonkeyReader(gltf) {
-    console.log('1');
-    dart_mesh = null;
-    dart_mesh = gltf.scene;
-    scene.add(dart_mesh);
-  }
-
-  loadData();
 
   //
 
@@ -106,13 +53,13 @@ function init() {
   // controllers
 
   controller1 = renderer.xr.getController(0);
-  controller1.addEventListener('selectstart', onSelectStart);
-  controller1.addEventListener('selectend', onSelectEnd);
+  // controller1.addEventListener('selectstart', onSelectStart);
+  // controller1.addEventListener('selectend', onSelectEnd);
   scene.add(controller1);
 
   controller2 = renderer.xr.getController(1);
-  controller2.addEventListener('selectstart', onSelectStart);
-  controller2.addEventListener('selectend', onSelectEnd);
+  // controller2.addEventListener('selectstart', onSelectStart);
+  // controller2.addEventListener('selectend', onSelectEnd);
   scene.add(controller2);
 
   raycaster = new THREE.Raycaster();
@@ -120,6 +67,10 @@ function init() {
   //
 
   window.addEventListener('resize', onWindowResize);
+
+  //
+
+  ammoMngr.setScene(scene);
 
 }
 
@@ -132,84 +83,84 @@ function onWindowResize() {
 
 }
 
-function onSelectStart(event) {
+// function onSelectStart(event) {
 
-  const controller = event.target;
+//   const controller = event.target;
 
-  const intersections = getIntersections(controller);
+//   const intersections = getIntersections(controller);
 
-  if (intersections.length > 0) {
+//   if (intersections.length > 0) {
 
-    const intersection = intersections[0];
+//     const intersection = intersections[0];
 
-    const object = intersection.object;
-    object.material.emissive.b = 1;
-    controller.attach(object);
+//     const object = intersection.object;
+//     object.material.emissive.b = 1;
+//     controller.attach(object);
 
-    controller.userData.selected = object;
+//     controller.userData.selected = object;
 
-  }
+//   }
 
-}
+// }
 
-function onSelectEnd(event) {
+// function onSelectEnd(event) {
 
-  const controller = event.target;
+//   const controller = event.target;
 
-  if (controller.userData.selected !== undefined) {
+//   if (controller.userData.selected !== undefined) {
 
-    const object = controller.userData.selected;
-    object.material.emissive.b = 0;
-    group.attach(object);
+//     const object = controller.userData.selected;
+//     object.material.emissive.b = 0;
+//     group.attach(object);
 
-    controller.userData.selected = undefined;
+//     controller.userData.selected = undefined;
 
-  }
+//   }
 
 
-}
+// }
 
-function getIntersections(controller) {
+// function getIntersections(controller) {
 
-  tempMatrix.identity().extractRotation(controller.matrixWorld);
+//   tempMatrix.identity().extractRotation(controller.matrixWorld);
 
-  raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
-  raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+//   raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+//   raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
 
-  return raycaster.intersectObjects(group.children, false);
+//   return raycaster.intersectObjects(group.children, false);
 
-}
+// }
 
-function intersectObjects(controller) {
+// function intersectObjects(controller) {
 
-  // Do not highlight when already selected
+//   // Do not highlight when already selected
 
-  if (controller.userData.selected !== undefined) return;
+//   if (controller.userData.selected !== undefined) return;
 
-  const intersections = getIntersections(controller);
+//   const intersections = getIntersections(controller);
 
-  if (intersections.length > 0) {
+//   if (intersections.length > 0) {
 
-    const intersection = intersections[0];
+//     const intersection = intersections[0];
 
-    const object = intersection.object;
-    object.material.emissive.r = 1;
-    intersected.push(object);
+//     const object = intersection.object;
+//     object.material.emissive.r = 1;
+//     intersected.push(object);
 
-  }
+//   }
 
-}
+// }
 
-function cleanIntersected() {
+// function cleanIntersected() {
 
-  while (intersected.length) {
+//   while (intersected.length) {
 
-    const object = intersected.pop();
-    object.material.emissive.r = 0;
+//     const object = intersected.pop();
+//     object.material.emissive.r = 0;
 
-  }
+//   }
 
-}
+// }
 
 //
 
@@ -220,12 +171,18 @@ function animate() {
 }
 
 function render() {
+  let delta = clock.getDelta();
+  let elapsed = clock.elapsedTime;
 
-  cleanIntersected();
+  // cleanIntersected();
 
-  intersectObjects(controller1);
-  intersectObjects(controller2);
+  // intersectObjects(controller1);
+  // intersectObjects(controller2);
+
+  ammoMngr.updateAmmosPositions(elapsed, delta);
 
   renderer.render(scene, camera);
 
 }
+
+setTimeout(() => ammoMngr.addAmmo(camera, 5000), 5000);
